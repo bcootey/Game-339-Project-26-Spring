d5172ff7fb86130eb1a383dcdb66447e5be78024
Braeden <cootey@chapman.edu>
2026-02-18 16:54:36 -0800

    Changed Unit Tests

--- src/Shared/Game339.Shared/Runtime/Services/Implementation/StringService.cs
+++ src/Shared/Game339.Shared/Runtime/Services/Implementation/StringService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using Game339.Shared.Diagnostics;
 
@@ -12,10 +13,17 @@ namespace Game339.Shared.Services.Implementation
             _log = log;
         }
 
-        public string Reverse(string input)
+         public string ReverseWords(string input)
         {
-            var output = new string(input.Reverse().ToArray());
-            _log.Info($"{nameof(StringService)}.{nameof(Reverse)} - {nameof(input)}: {input} - {nameof(output)}: {output}");
+            if (string.IsNullOrWhiteSpace(input))
+                return input;
+
+            var output = string.Join(" ",
+                input.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                     .Reverse());
+
+            _log.Info($"{nameof(StringService)}.{nameof(ReverseWords)} - {nameof(input)}: {input} - {nameof(output)}: {output}");
+
             return output;
         }
     }
--- src/Shared/Game339.Tests/StringServiceTests.cs
+++ src/Shared/Game339.Tests/StringServiceTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Game339.Shared.Services.Implementation;
 using NUnit.Framework;
 
@@ -13,23 +14,25 @@ public class StringServiceTests
         _svc = new StringService(EmptyGameLog.Instance);
     }
 
-    [TestCase("hello", "olleh")]
-    [TestCase("", "")]
-    [TestCase("a", "a")]
-    [TestCase("racecar", "racecar")]
-    public void Reverse_ReturnsExpectedString(string input, string expected)
+    [TestCase("hello", "hello")]                 // single word stays the same
+    [TestCase("", "")]                           // empty stays empty
+    [TestCase("a", "a")]                         // single word stays the same
+    [TestCase("racecar", "racecar")]             // single word stays the same
+    [TestCase("hello world", "world hello")]     // two words swap
+    [TestCase("one two three", "three two one")] // multiple words reverse
+    public void ReverseWords_ReturnsExpectedString(string input, string expected)
     {
         // Act
-        var result = _svc.Reverse(input);
+        var result = _svc.ReverseWords(input);
 
         // Assert
         Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
-    public void Reverse_NullString_ThrowsArgumentNullException()
+    public void ReverseWords_NullString_ThrowsArgumentNullException()
     {
         // Act & Assert
-        Assert.Throws<System.ArgumentNullException>(() => _svc.Reverse(null));
+        Assert.Throws<ArgumentNullException>(() => _svc.ReverseWords(null));
     }
 }
